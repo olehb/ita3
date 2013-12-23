5643cb32cb738ab3b95f621ea7b540947f1d6270
Oleg Boiko <oleh.boiko@gmail.com>
2013-12-22 21:24:16 -0800

    Syntax improvements to merge sort

--- ch2/ex_2.3-1_merge_sort.rb
+++ ch2/ex_2.3-1_merge_sort.rb
@@ -1,8 +1,10 @@
 def merge a, p, r, q
-	sorted_a = Array.new(q-p+1)
+	n = q-p+1
+	sorted_a = Array.new(n)
 	i = p
 	j = r+1
 	k = 0
+
 	while i <= r and j <= q
 		if a[i] < a[j]
 			sorted_a[k] = a[i]
@@ -13,26 +15,27 @@ def merge a, p, r, q
 		end
 		k += 1
 	end
-	
+
 	if i <= r
-		sorted_a[k, sorted_a.length-k] = a[i, r-i+1]
+		sorted_a[k, n-k] = a[i, r-i+1]
 	elsif j <= q
-		sorted_a[k, sorted_a.length-k] = a[j, q-j+1]
+		sorted_a[k, n-k] = a[j, q-j+1]
 	end
-	
-	a[p,sorted_a.length]=sorted_a
+
+	a[p, n] = sorted_a
 end
 
 def merge_sort a, p, q
-	if p != q
-		r = (q-p) / 2
-		merge_sort a, p, p+r
-		merge_sort a, p+r+1, q
-		merge a, p, p+r, q
+	if p < q
+		r = p+(q-p)/2
+		merge_sort a, p, r 
+		merge_sort a, r+1, q
+		merge a, p, r, q
 	end
 end
 
-a = (0..30).to_a.shuffle
+a = (1..20000000).to_a.shuffle
 puts a.to_s
+
 merge_sort a, 0, a.length-1
 puts a.to_s
