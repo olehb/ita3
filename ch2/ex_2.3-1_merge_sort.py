cdac61ef7cbdf14f880335f9cf305225b1d890dd
Oleg Boiko <oleh.boiko@gmail.com>
2013-12-28 14:49:53 -0800

    Merge sort in Python, dump implementation

---
+++ ch2/ex_2.3-1_merge_sort.py
@@ -0,0 +1,34 @@
+import random
+
+def merge(a, p, r, q):
+	sorted_a = []
+	i = p
+	j = r+1
+	while i<=r and j <=q:
+		if a[i] < a[j]:
+			sorted_a.append(a[i])
+			i += 1
+		else:
+			sorted_a.append(a[j])
+			j += 1
+	
+	if i<=r:
+		sorted_a = sorted_a + a[i:r+1]
+	
+	if j<=q:
+		sorted_a = sorted_a + a[j:q+1]
+
+	a[p:q+1] = sorted_a
+	return a
+
+def merge_sort(a, p, q):
+	if  p<q:
+		r = (p+q)/2
+		merge_sort(a, p, r)
+		merge_sort(a, r+1, q)
+		merge(a, p, r, q)
+	return a
+
+a = [int(1000*random.random()) for i in xrange(20000000)]
+merge_sort(a, 0, len(a)-1)
+print a
