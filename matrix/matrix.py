674b8f767b5e69d996f620e7c2a844f40a13afe5
Oleg Boiko <oleh.boiko@gmail.com>
2014-11-29 15:32:19 -0800

    Creating Submatrix class

--- matrix/matrix.py
+++ matrix/matrix.py
@@ -1,46 +1,28 @@
-from __future__ import print_function
-
 def multiple_submatrices(m1, i1, j1, m2, i2, j2, n, m, p):
     return Matrix(list(sum(m1(i1+i,j1+x)*m2(i2+x,j2+j) for x in range(m)) for i in range(n) for j in range(p)), n, p)
 
-class Matrix:
-    def __init__(self, a, n, m):
-        assert n*m == len(a)
-        self.a = a
-        self.n = n
-        self.m = m
-
-    def getRow(self, i):
-        j = self.m*i
-        return self.a[j:j+self.m]
-
-    def getColumn(self, j):
-        return list(self.a[i*self.m+j] for i in range(self.n))
+class AbstractMatrix:
+    def __init__(self, nRows, nCols):
+        self.nRows = nRows
+        self.nCols = nCols
 
     def getDimensions(self):
-        return (self.n, self.m)
-
-    def __iter__(self):
-        for e in self.a: 
-            yield e
-
-    def __call__(self, i, j):
-        return self.a[self.m*i + j]
+        return (self.nRows, self.nCols)
 
     def __add__(self, m):
-        return Matrix(list(self(i,j) + m(i,j) for i in range(self.m) for j in range(self.n)), self.n, self.m)
+        return Matrix(list(self(i,j) + m(i,j) for i in range(self.nCols) for j in range(self.nRows)), self.nRows, self.nCols)
 
     def __sub__(self, m):
-        return Matrix(list(self(i,j) - m(i,j) for i in range(self.m) for j in range(self.n)), self.n, self.m)
+        return Matrix(list(self(i,j) - m(i,j) for i in range(self.nCols) for j in range(self.nRows)), self.nRows, self.nCols)
 
     def __repr__(self):
-        return '\n'.join(' '.join(map(str, self.getRow(i))) for i in range(self.n))
+        return '\n'.join(' '.join(map(str, self.getRow(i))) for i in range(self.nRows))
 
     def __eq__(self, m):
-        return self.getDimensions() == m.getDimensions() and all(self(i,j) == m(i,j) for i in range(self.n) for j in range(self.m))
+        return self.getDimensions() == m.getDimensions() and all(self(i,j) == m(i,j) for i in range(self.nRows) for j in range(self.nCols))
 
     def __mul__(self, s):
-        if isinstance(s, Matrix):
+        if isinstance(s, self.__class__):
             return self.multipleWithMatrix(s)
         else:
             return self.multipleWithScalar(s)
@@ -52,12 +34,54 @@ class Matrix:
         return self*-1
 
     def multipleWithScalar(self, s):
-        return Matrix([e*s for e in self], self.n, self.m)
+        return Matrix([e*s for e in self], self.nRows, self.nCols)
 
     def multipleWithMatrix(self, m):
         (n1, m1) = m.getDimensions()
-        a = list(sum(self(i,x)*m(x,j) for x in range(self.m)) for i in range(self.n) for j in range(m1))
-        return Matrix(a, self.n, m1)
+        a = list(sum(self(i,x)*m(x,j) for x in range(self.nCols)) for i in range(self.nRows) for j in range(m1))
+        return Matrix(a, self.nRows, m1)
+
+
+class Submatrix(AbstractMatrix):
+    def __init__(self, p, i, j, n, m):
+        AbstractMatrix.__init__(self, n, m)
+        self.__p = p
+        self.__i = i
+        self.__j = j
+
+    def getRow(self, i):
+        return self.__p.getRow(self.__i+i)[self.__j:self.__j+self.nCols]
+
+    def getColumn(self, j):
+        return self.__p.getColumn(self.__j+j)[self.__i:self.__i+self.nRows]
+
+    def __iter__(self):
+        for i in range(self.nRows):
+            for j in self.__p.getRow(self.__i+i):
+                yield j
+
+    def __call__(self, i, j):
+        return self.__p(self.__i+i, self.__j+j)
+
+
+class Matrix(AbstractMatrix):
+    def __init__(self, a, n, m):
+        assert n*m == len(a), "%sx%s != %s" % (n, m, len(a))
+        AbstractMatrix.__init__(self, n, m)
+        self.__a = a
+
+    def getRow(self, i):
+        j = self.nCols*i
+        return self.__a[j:j+self.nCols]
+
+    def getColumn(self, j):
+        return list(self.__a[i*self.nCols+j] for i in range(self.nRows))
+
+    def __iter__(self):
+        return iter(a)
+
+    def __call__(self, i, j):
+        return self.__a[self.nCols*i + j]
 
 
 if __name__ == '__main__':
@@ -80,4 +104,4 @@ if __name__ == '__main__':
 
     assert m1*m2 == m3, m1*m2
     assert multiple_submatrices(m1, 0, 1, m2, 1, 1, 2, 1, 3) == Matrix([3, 5], 2, 1)*Matrix([12, 14, 16], 1, 3)
-
+    assert Submatrix(m1, 0, 1, 2, 1)*Submatrix(m2, 1, 1, 1, 3) == Matrix([3, 5], 2, 1)*Matrix([12, 14, 16], 1, 3)
